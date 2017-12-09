0382bae62c0c231f14b930ef02ea05dba024084f
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-12-09 01:25:42 -0800

    done on index.html

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-
+app.use(express.static("public"));
 
 require("./routes/html-routes")(app);
 
